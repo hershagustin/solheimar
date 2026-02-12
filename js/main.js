dffd9df1d4e8902607c39491fad9139930133e61
Hershey Agustin <hershagustin@gmail.com>
2026-02-12 04:05:02 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click', function(){
+    document.querySelector('nav').classList.toggle('show-nav');
+    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+
+});
